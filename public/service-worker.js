b1aa5858f798143c87538cec2adbf3fa4e82e3e1
Michael Rosa-Vourazeris <mrosavourazeris@gmail.com>
2021-03-22 16:28:03 -0500

    Working on finishing the service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,14 @@
 console.log("Service worker works!")
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+    "/index.js",
+    "/manifest.json",
+    "/styles.css",
+    "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+]
+
+const CACHE_NAME = "static-cache-v2"
+const DATA_CACHE_NAME = "data-cache-v1"
